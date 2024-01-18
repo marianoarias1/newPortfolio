668feaab2479a31a509a0d54e2acdca12f03f0a8
marianoarias1 <marianoarias123@hotmail.com>
2024-01-18 11:15:09 -0300

    change projects

--- src/projects.js
+++ src/projects.js
@@ -1,27 +1,28 @@
 export const projects =[
     {
         id: 1,
-        name: "Gentleman Desing",
-        desc: "Un fake e-commerce realizado con HTML, SASS y BOOSTRAP. Este proyecto fue hecho como trabajo integrador durante mi cursada en CODERHOUSE.",
-        url: "https://gentleman-design.vercel.app/index.html",
-        img: "../images/projectIMG/gentlemandesing.png",
-        code: "https://github.com/marianoarias1/PreEntrega2-MarianoArias.git",
+        name: "TipyRun",
+        desc: "Un juego para practicar mecanografía, fue realizado con Javascript tanto para el manejo del DOM, como las funciones que miden los datos del usuario, estos últimos se muestran al finalizar el test.",
+        url: "https://typi-run.vercel.app/",
+        img: "../images/projectIMG/tipyrun.png",
+        code: "https://github.com/marianoarias1/TypiRun.git",
 
     },
     {
         id: 2,
-        name: "API Simpsons",
-        desc: "Una aplicación web que utiliza una API externa para hacer consultas, las cuales nos traen como respuesta una imagen, frase y nombre del personaje de Los Simposon que hayamos buscado.",
-        url: "https://api-simpons.vercel.app/",
-        img: "../images/projectIMG/apisimpson.png",
-        code: "https://github.com/marianoarias1/api-simpons.git",
-    },
-    {
-        id: 3,
         name: "Shiftkeys",
         desc: "Un fake e-commerce realizado durante mi cursada en NUCBA, utilizando JS para manipular el DOM y también para persistir los datos del usuario como el carrito.",
         url: "https://shiftkeys.vercel.app/",
         img: "../images/projectIMG/shiftkeys.png",
         code: "https://github.com/marianoarias1/ShiftKeys.git",
     },
+    {
+        id: 3,
+        name: "Pomodor Timer",
+        desc: "Una aplicación de un temporizador pomodoro, el cual tiene dos tiempos, uno para la tarea principal, y uno para el tiempo de descanso, realizado con Javascript para el manejo del DOM y los intervalos de tiempo.",
+        url: "https://pomodoro-timer-lemon-one.vercel.app/",
+        img: "../images/projectIMG/pomodorotimer.png",
+        code: "https://github.com/marianoarias1/PomodoroTimer.git",
+    },
+
 ]
