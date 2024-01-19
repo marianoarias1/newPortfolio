6abc1a94ef1c7c20f02ffd23a1d135226b080de5
marianoarias1 <marianoarias123@hotmail.com>
2024-01-19 12:40:17 -0300

    change project pictures

--- src/projects.js
+++ src/projects.js
@@ -4,7 +4,7 @@ export const projects =[
         name: "TipyRun",
         desc: "Un juego para practicar mecanografía, fue realizado con Javascript tanto para el manejo del DOM, como las funciones que miden los datos del usuario, estos últimos se muestran al finalizar el test.",
         url: "https://typi-run.vercel.app/",
-        img: "../images/projectIMG/tipyrun.png",
+        img: "../images/projectIMG/tipyrun-img.png",
         code: "https://github.com/marianoarias1/TypiRun.git",
 
     },
@@ -13,15 +13,15 @@ export const projects =[
         name: "Shiftkeys",
         desc: "Un fake e-commerce realizado durante mi cursada en NUCBA, utilizando JS para manipular el DOM y también para persistir los datos del usuario como el carrito.",
         url: "https://shiftkeys.vercel.app/",
-        img: "../images/projectIMG/shiftkeys.png",
+        img: "../images/projectIMG/shiftkeys-img1.png",
         code: "https://github.com/marianoarias1/ShiftKeys.git",
     },
     {
         id: 3,
         name: "Pomodor Timer",
         desc: "Una aplicación de un temporizador pomodoro, el cual tiene dos tiempos, uno para la tarea principal, y uno para el tiempo de descanso, realizado con Javascript para el manejo del DOM y los intervalos de tiempo.",
-        url: "https://pomodoro-timer-lemon-one.vercel.app/",
-        img: "../images/projectIMG/pomodorotimer1.png",
+        url: "https://timer-mariano-arias.vercel.app/",
+        img: "../images/projectIMG/pomodorotimer-img1.png",
         code: "https://github.com/marianoarias1/PomodoroTimer.git",
     },
 
